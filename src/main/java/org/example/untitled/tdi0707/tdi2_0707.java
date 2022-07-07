0ba7029a22e2a4e0e9a797b7f6c88051495189f9
hyujikoh <dvum0045@gmail.com>
2022-07-07 13:47:39 +0900

    부연설명 추가

---
+++ src/main/java/org/example/untitled/tdi0707/tdi2_0707.java
@@ -0,0 +1,30 @@
+package org.example.untitled.tdi0707;
+//code.oa.gg/java8/938
+// 문제 : 아래와 같이 출력되도록 해주세요. init() 메세드에 의해서 출력되어야 합니다.
+// 조건 : 사람 클래스의 생성자는 사용할 수 없습니다.
+// 힌트 : 모든 생성자는 자신의 일을 하기 전에 super(); 부터 실행합니다.
+// 힌트 : 모든 생성자는 자신의 일을 하기 전에 부모클래스의 생성자 부터 실행합니다.
+public class tdi2_0707 {
+    public static void main(String[] args) {
+        new 사람1();
+        // 출력 : 사람이 초기화 됩니다.
+    }
+}
+
+class 동물1 {
+    동물1(){
+
+        init();
+    }
+    void init(){
+
+    }
+}
+
+class 사람1 extends 동물1 {
+
+    void init() {
+
+        System.out.println("사람이 초기화 됩니다.");
+    }
+}
