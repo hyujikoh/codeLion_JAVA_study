6f5ee9dc3a2a2ed99662a3c22ae240c30cf4e7b5
hyujikoh <dvum0045@gmail.com>
2022-07-07 13:29:25 +0900

    0707 super로 부모 생성자 사용(매개변수 사용)

---
+++ src/main/java/org/example/untitled/tdi0707/tdi_0707.java
@@ -0,0 +1,27 @@
+package org.example.untitled.tdi0707;
+// 문제 : 아래가 오류없이 실행되도록 해주세요.
+// 조건 : 사람 클래스만 수정할 수 있습니다.
+public class tdi_0707 {
+    public static void main(String[] args) {
+        new 사람();
+    }
+}
+class 동물 {
+    동물(String 이름) {
+        System.out.println("동물(String 이름) 실행됨!");
+        System.out.println("이름 : " + 이름);
+    }
+    동물(String 이름, int 나이) {
+        System.out.println("동물(String 이름, int 나이) 실행됨!");
+        System.out.println("이름 : " + 이름);
+        System.out.println("나이 : " + 나이);
+    }
+}
+class 사람 extends 동물 {
+
+    사람(){
+        super("철수",20);
+
+
+    };
+}
