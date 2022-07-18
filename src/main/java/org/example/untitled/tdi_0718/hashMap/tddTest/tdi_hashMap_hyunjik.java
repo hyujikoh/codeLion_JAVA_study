5a1de9981ad6450fde5605afc0de20412f43f946
hyujikoh <dvum0045@gmail.com>
2022-07-18 13:45:33 +0900

    챕터 33 : HashMap Tdd 테스트 스스로 해보기 제너릭이_가능하다_2단계

--- src/main/java/org/example/untitled/tdi_0718/hashMap/tddTest/tdi_hashMap_hyunjik.java
+++ src/main/java/org/example/untitled/tdi_0718/hashMap/tddTest/tdi_hashMap_hyunjik.java
@@ -2,7 +2,7 @@ package org.example.untitled.tdi_0718.hashMap.tddTest;
 
 public class tdi_hashMap_hyunjik {
 }
-class HashMap{
+class HashMap<K,V>{
 
 
 }
--- src/test/java/org/example/untitled/tdi_0718/hashMap/tddTest/AppTest.java
+++ src/test/java/org/example/untitled/tdi_0718/hashMap/tddTest/AppTest.java
@@ -16,6 +16,9 @@ class HashMapTest {
 
         HashMap map = new HashMap();
     }
-
+    @Test
+    void 제너릭이_가능하다_2단계() {
+        HashMap<String, Integer> map = new HashMap<>();
+    }
 
 }
