f7fb7fdadd20753bed7c393b2946eecb0c7ee3ad
hyujikoh <dvum0045@gmail.com>
2022-07-18 13:44:17 +0900

    챕터 33 : HashMap Tdd 테스트 스스로 해보기

---
+++ src/main/java/org/example/untitled/tdi_0718/hashMap/tddTest/tdi_hashMap_hyunjik.java
@@ -0,0 +1,8 @@
+package org.example.untitled.tdi_0718.hashMap.tddTest;
+
+public class tdi_hashMap_hyunjik {
+}
+class HashMap{
+
+
+}
---
+++ src/test/java/org/example/untitled/tdi_0718/hashMap/tddTest/AppTest.java
@@ -0,0 +1,21 @@
+package org.example.untitled.tdi_0718.hashMap.tddTest;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+}
+
+class HashMapTest {
+    @Test
+    void hashmap_클래스_존재_유무(){
+
+        HashMap map = new HashMap();
+    }
+
+
+}
